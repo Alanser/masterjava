005b940dff4dc8fe633eba716dce45d94a3bd4ee
Anton Alputov <alputov@gmail.com>
2020-07-12 13:22:10 +0600

    Add DAO cities, groups and projects. Add DAO tests

---
+++ persist/src/main/java/ru/javaops/masterjava/persist/dao/GroupDao.java
@@ -0,0 +1,36 @@
+package ru.javaops.masterjava.persist.dao;
+
+import com.bertoncelj.jdbi.entitymapper.EntityMapperFactory;
+import one.util.streamex.StreamEx;
+import org.skife.jdbi.v2.sqlobject.*;
+import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapperFactory;
+import ru.javaops.masterjava.persist.model.City;
+import ru.javaops.masterjava.persist.model.Group;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+@RegisterMapperFactory(EntityMapperFactory.class)
+public abstract class GroupDao implements AbstractDao {
+
+    @SqlUpdate("TRUNCATE groups CASCADE")
+    @Override
+    public abstract void clean();
+
+    @SqlQuery("SELECT * FROM groups ORDER BY name")
+    public abstract List<Group> getAll();
+
+    public Map<String, Group> getAsMap() {
+        return StreamEx.of(getAll()).toMap(Group::getName, c -> c);
+    }
+
+    @SqlUpdate("INSERT INTO groups (name, type, project_id) VALUES (:name, CAST(:type AS GROUP_TYPE), :projectId)")
+    @GetGeneratedKeys
+    public abstract int insertGeneratedId(@BindBean Group group);
+
+    public void insert(Group group) {
+        int id = insertGeneratedId(group);
+        group.setId(id);
+    }
+}
---
+++ persist/src/test/java/ru/javaops/masterjava/persist/dao/ProjectDaoTest.java
@@ -0,0 +1,29 @@
+package ru.javaops.masterjava.persist.dao;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import ru.javaops.masterjava.persist.ProjectTestData;
+import ru.javaops.masterjava.persist.model.Project;
+
+import java.util.Map;
+
+import static ru.javaops.masterjava.persist.ProjectTestData.PROJECTS;
+
+public class ProjectDaoTest extends AbstractDaoTest<ProjectDao> {
+    public ProjectDaoTest() {
+        super(ProjectDao.class);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        ProjectTestData.setUp();
+    }
+
+    @Test
+    public void getAll() throws Exception {
+        final Map<String, Project> projects = dao.getAsMap();
+        Assert.assertEquals(PROJECTS, projects);
+        System.out.println(projects.values());
+    }
+}
