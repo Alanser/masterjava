0a9fef8860bb4d4d4972988a876dc0f5c3dfd397
Anton Alputov <alputov@gmail.com>
2020-07-14 20:54:06 +0600

    add MailSender

--- services/mail-api/src/main/java/ru/javaops/masterjava/service/mail/Addressee.java
+++ services/mail-api/src/main/java/ru/javaops/masterjava/service/mail/Addressee.java
@@ -3,6 +3,7 @@ package ru.javaops.masterjava.service.mail;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 /**
  * gkislin
@@ -12,6 +13,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Addressee {
+
+    @NonNull
     private String email;
     private String name;
+
+    public Addressee(String email) {
+        this(email, null);
+    }
 }
