070efe6327f83f27a5773091e6db8de87112ef1b
Anton Alputov <alputov@gmail.com>
2020-08-07 10:01:35 +0600

    SOAP exceptions

--- services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/MailServiceExecutor.java
+++ services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/MailServiceExecutor.java
@@ -2,6 +2,9 @@ package ru.javaops.masterjava.service.mail;
 
 import lombok.extern.slf4j.Slf4j;
 import one.util.streamex.StreamEx;
+import ru.javaops.masterjava.ExceptionType;
+import ru.javaops.web.WebStateException;
+import ru.javaops.web.WsClient;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -14,56 +17,59 @@ public class MailServiceExecutor {
 
     private static final String INTERRUPTED_BY_FAULTS_NUMBER = "+++ Interrupted by faults number";
     private static final String INTERRUPTED_BY_TIMEOUT = "+++ Interrupted by timeout";
-    private static final String INTERRUPTED_EXCEPTION = "+++ InterruptedException";
 
     private static final ExecutorService mailExecutor = Executors.newFixedThreadPool(8);
 
-    public static GroupResult sendBulk(final Set<Addressee> addressees, final String subject, final String body) {
+    public static GroupResult sendBulk(final Set<Addressee> addressees, final String subject, final String body) throws WebStateException {
 
-       final CompletionService<MailResult> completionService = new ExecutorCompletionService<>(mailExecutor);
+        final CompletionService<MailResult> completionService = new ExecutorCompletionService<>(mailExecutor);
 
         List<Future<MailResult>> futures = StreamEx.of(addressees)
                 .map(addressee -> completionService.submit(() -> MailSender.sendTo(addressee, subject, body)))
                 .toList();
 
-               return new Callable<GroupResult>() {
-                    private int success = 0;
-                    private List<MailResult> failed = new ArrayList<>();
+        return new Callable<GroupResult>() {
+            private int success = 0;
+            private List<MailResult> failed = new ArrayList<>();
 
-                            @Override
-                    public GroupResult call() {
-                            while (!futures.isEmpty()) {
-                                    try {
-                                            Future<MailResult> future = completionService.poll(10, TimeUnit.SECONDS);
-                                            if (future == null) {
-                                                   return cancelWithFail(INTERRUPTED_BY_TIMEOUT);
-                                                }
-                                           futures.remove(future);
-                                            MailResult mailResult = future.get();
-                                            if (mailResult.isOk()) {
-                                                    success++;
-                                                } else {
-                                                    failed.add(mailResult);
-                                                    if (failed.size() >= 5) {
-                                                            return cancelWithFail(INTERRUPTED_BY_FAULTS_NUMBER);
-                                                        }
-                                                }
-                                        } catch (ExecutionException e) {
-                                            return cancelWithFail(e.getCause().toString());
-                                        } catch (InterruptedException e) {
-                                            return cancelWithFail(INTERRUPTED_EXCEPTION);
-                                        }
-                                }
-
-                                GroupResult groupResult = new GroupResult(success, failed, null);
-                                log.info("groupResult: {}", groupResult);
-                                return groupResult;
+            @Override
+            public GroupResult call() throws WebStateException {
+                while (!futures.isEmpty()) {
+                    try {
+                        Future<MailResult> future = completionService.poll(10, TimeUnit.SECONDS);
+                        if (future == null) {
+                            cancel(INTERRUPTED_BY_TIMEOUT, null);
                         }
-
-                    private GroupResult cancelWithFail(String cause) {
-                            futures.forEach(f -> f.cancel(true));
-                            return new GroupResult(success, failed, cause);
+                        futures.remove(future);
+                        MailResult mailResult = future.get();
+                        if (mailResult.isOk()) {
+                            success++;
+                        } else {
+                            failed.add(mailResult);
+                            if (failed.size() >= 5) {
+                                cancel(INTERRUPTED_BY_FAULTS_NUMBER, null);
+                            }
                         }
-                }.call();
+                    } catch (ExecutionException e) {
+                        cancel(null, e.getCause());
+                    } catch (InterruptedException e) {
+                        cancel(INTERRUPTED_BY_TIMEOUT, null);
+                    }
+                }
+
+                GroupResult groupResult = new GroupResult(success, failed);
+                log.info("groupResult: {}", groupResult);
+                return groupResult;
+            }
+
+            private void cancel(String cause, Throwable t) throws WebStateException {
+                futures.forEach(f -> f.cancel(true));
+                if (cause != null) {
+                    throw new WebStateException(cause, ExceptionType.EMAIL);
+                } else {
+                    throw WsClient.getWebStateException(t, ExceptionType.EMAIL);
+                }
+            }
+        }.call();
     }
 }
